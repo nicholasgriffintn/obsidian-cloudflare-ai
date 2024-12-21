f865049bfdf7d2967a6f756db63a4af9e3923e78
Nicholas Griffin <nick@undefined.computer>
2024-12-21 19:50:04 +0000

    fix: versions build

--- scripts/bump-version.ts
+++ scripts/bump-version.ts
@@ -7,12 +7,12 @@ if (!targetVersion) {
 }
 
 // read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+let manifest = JSON.parse(readFileSync("public/manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+writeFileSync("public/manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+let versions = JSON.parse(readFileSync("public/versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+writeFileSync("public/versions.json", JSON.stringify(versions, null, "\t"));
